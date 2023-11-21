96510e615ac39df5c3d3e2797d768dda8268dbea
“Arunthamizh” <arunthamizh.azhagiri@twilightitsolutions.com>
2023-11-21 11:25:38 +0530

    lear: useEffect and its use

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // ! this will create a infinate loop
+  // ! Because when update the useState the function will be called again and react render the component(infinate loop)
+  // if(storedUserLoggedInInformation === '1'){
+  //   setIsLoggedIn(true);
+  // }
+
+  // ! This will fix the infinate loop by using useEffect
+  // ! useEffect will be called only once when the component is mounted and after component re-evaluation
+  // ! If we pass an empty array as the second argument, useEffect will be called every time the component is re-rendered
+  // ! If we pass an array with a value, useEffect will be called only when the value in the array changes
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+    if(storedUserLoggedInInformation === '1'){
+      setIsLoggedIn(true);
+    }
+  }, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
