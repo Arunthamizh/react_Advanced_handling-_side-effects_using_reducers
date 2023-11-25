8c81a72103eb06514b4b3e013fee581c2b4d730c
Arunthamizh-laptop <workattwilight@gmail.com>
2023-11-25 18:24:25 +0530

    10 video useReducer and useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   // ! this will create a infinate loop
@@ -15,13 +13,16 @@ function App() {
   //   setIsLoggedIn(true);
   // }
 
+  // ! useEffect will run after component re-evaluation is done by react
+  // ! If dependency('[]') are not mentioned the useEffect will run only once when app starts
+
   // ! This will fix the infinate loop by using useEffect
   // ! useEffect will be called only once when the component is mounted and after component re-evaluation
   // ! If we pass an empty array as the second argument, useEffect will be called every time the component is re-rendered
   // ! If we pass an array with a value, useEffect will be called only when the value in the array changes
   useEffect(() => {
     const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
-    if(storedUserLoggedInInformation === '1'){
+    if (storedUserLoggedInInformation === "1") {
       setIsLoggedIn(true);
     }
   }, []);
@@ -29,12 +30,12 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
-    localStorage.setItem('isLoggedIn', '1');
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn');
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -16,14 +16,41 @@ const emailReducer = (state, action) => {
   return { value: "", isValid: false };
 };
 
+const passwordReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.trim().length > 6 };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.trim().length > 6 };
+  }
+  return { value: "", isValid: false };
+}
+
 const Login = (props) => {
-  // * commented because we are using useReducer
+  // ! commented because we are using useReducer
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
 
-  const [enteredPassword, setEnteredPassword] = useState("");
-  const [passwordIsValid, setPasswordIsValid] = useState();
+  //  ! commented because we are using useReducer
+  // const [enteredPassword, setEnteredPassword] = useState("");
+  // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  // * useReducer
+  // ! The useReducer function will run whenever one of the dependencies changes and every component render cycle
+  // ! useReducer is alternative to useEffect to overcome the limitation of useEffect
+  // ! In useState we need two states to handle state value and state validity
+  // ! In useReducer we need only one state to handle state value and state validity
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: false,
+  });
+
+  const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
+    value: "",
+    isValid: false
+  })
 
   // * useEffect
   // ! The useEffect function will run whenever one of the dependencies changes and every component render cycle
@@ -44,48 +71,45 @@ const Login = (props) => {
   // ! useEffect
   // * commented because we are using useReducer
 
-  // useEffect(() => {
-  //   const identifer =  setTimeout(() => {
-  //     console.log('Checking form validity!');
-  //     setFormIsValid(
-  //       enteredEmail.includes('@') && enteredPassword.trim().length > 6
-  //     );
-  //   }, 500);
+// !below code is for object destructuring with alias name
+const { isValid: emailIsValid } = emailState;
+const { isValid: passwordIsValid } = passwordState;
 
-  //   // * useEffect cleanup function
-  //   // ! It will be executed before the useEffect function executes
-  //   // ! Cleanup function will not called at first time.
+  useEffect(() => {
+    const identifier =  setTimeout(() => {
+      console.log('Checking form validity!');
+      setFormIsValid(
+        // emailState.isValid && passwordState.isValid
+        emailIsValid && passwordIsValid
+      );
+    }, 500);
 
-  //   return () => {
-  //     console.log('Clean up');
-  //     identifer && clearTimeout(identifer);
-  //   }
-  // },[enteredEmail, enteredPassword])
+    // * useEffect cleanup function
+    // ! It will be executed before the useEffect function executes
+    // ! Cleanup function will not called at first time.
 
-  // * useReducer
-  // ! The useReducer function will run whenever one of the dependencies changes and every component render cycle
-  // ! useReducer is alternative to useEffect to overcome the limitation of useEffect
-  // ! In useState we need two states to handle state value and state validity
-  // ! In useReducer we need only one state to handle state value and state validity
+    return () => {
+      console.log('Clean up');
+      identifier && clearTimeout(identifier);
+    }
+  },[emailIsValid, passwordIsValid])
 
-  const [emailState, dispatchEmail] = useReducer(emailReducer, {
-    value: "",
-    isValid: false,
-  });
+
 
   const emailChangeHandler = (event) => {
     // setEnteredEmail(event.target.value);
     dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
     // setFormIsValid(
-    //   event.target.value.includes('@') && enteredPassword.trim().length > 6
+    //   event.target.value.includes('@') && passwordState.isValid
     // );
   };
 
   const passwordChangeHandler = (event) => {
-    setEnteredPassword(event.target.value);
-
-    setFormIsValid(event.target.value.trim().length > 6 && emailState.isValid);
+    // setEnteredPassword(event.target.value);
+    // setFormIsValid(event.target.value.trim().length > 6 && emailState.isValid);
+    //  ! The above code was commented because we are using useReducer
+    dispatchPassword({ type: "USER_INPUT", val: event.target.value });
   };
 
   const validateEmailHandler = () => {
@@ -94,12 +118,14 @@ const Login = (props) => {
   };
 
   const validatePasswordHandler = () => {
-    setPasswordIsValid(enteredPassword.trim().length > 6);
+    // setPasswordIsValid(enteredPassword.trim().length > 6);
+
+    dispatchPassword({ type: "INPUT_BLUR" });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, enteredPassword);
+    props.onLogin(emailState.value, passwordState.value);
   };
 
   return (
@@ -121,14 +147,14 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ""
+            passwordState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
-            value={enteredPassword}
+            value={passwordState.value}
             onChange={passwordChangeHandler}
             onBlur={validatePasswordHandler}
           />
